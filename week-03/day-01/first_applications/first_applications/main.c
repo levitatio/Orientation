cd10538b999f869a007d508cf5d71011f5a8da5e
Balogh Levente <balogh.levente.istvan@gmail.com>
2019-11-04 17:32:49 +0100

    finish button-press counter task

--- week-03/day-01/first_applications/first_applications/main.c
+++ week-03/day-01/first_applications/first_applications/main.c
@@ -17,18 +17,26 @@ void myDelay (uint32_t millisec)
 int main(void)
 {
 	DDRB &= 0b01111111;
-	PORTB |= 0b10000000; 
+	PORTB = 0b10000000; 
 	uint32_t temp = PINB & 0x80;
     /* Replace with your application code */
     while (1) 
     {
 		DDRB |= 0x20;
-		if (!(PINB & 0x80) || temp == 0 ){
-			PORTB = PORTB ^ 0X20;
+		uint8_t counter = 0;
+		//if (!(PINB & 0x80) || temp == 0 ){
+		if (!(PINB & 0x80) && counter < 5)
 			//myDelay(500);
+			PORTB = PORTB ^ 0X20;
 			_delay_ms(500);
-			temp = 0;
-		}
+			
+			if (counter == 4){
+			 PORTB = 0x20;
+			}else {
+				counter++;	
+			}
+			
+		
     }
 }
 
---
+++ week-03/day-01/new_project/new_project/main.c
@@ -0,0 +1,31 @@
+/*
+ * new_project.c
+ *
+ * Created: 2019. 11. 04. 16:19:53
+ * Author : CFY
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+	uint8_t counter = 0;
+	uint8_t butten_toggling_flag = 0;
+	
+	DDRB = 0x20;
+    /* Replace with your application code */
+    while (1) 
+    {
+		if ((PINB & 0x80) && butten_toggling_flag){
+				butten_toggling_flag = 0;
+			}
+		if (!(PINB & 0x80) && !butten_toggling_flag && counter < 5){
+			counter++;
+			butten_toggling_flag = 1;
+		}
+		
+		if (counter == 5) PORTB = 0x20;
+    }
+}
+
