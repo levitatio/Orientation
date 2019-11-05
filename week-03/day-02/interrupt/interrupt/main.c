f0ffc02ee0d2cb0ee9664eb23ce1d171f1e16527
Balogh Levente <balogh.levente.istvan@gmail.com>
2019-11-05 15:02:58 +0100

    finish interrupt task

---
+++ week-03/day-02/interrupt/interrupt/main.c
@@ -0,0 +1,33 @@
+/*
+ * interrupt.c
+ *
+ * Created: 2019. 11. 05. 14:20:10
+ * Author : CFY
+ */ 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+ISR(PCINT0_vect)
+{
+	PORTB ^= 0x20;
+}
+
+
+
+int main(void)
+{
+    /* Replace with your application code */
+	
+	DDRB = 0x40;
+	
+	PCICR = 0b00000001;
+	PCMSK0 = 0b10000000;
+	sei();
+    while (1) 
+    {
+		
+		
+    }
+}
+
